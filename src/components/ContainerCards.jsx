a4073496d103eb112606b40c7b863a3bc3c6707c
jhoancanchila <canchila.jhoan@gmail.com>
2024-03-13 19:13:08 -0500

    fetch(api-store):fetch product data

--- src/components/ContainerCards.jsx
+++ src/components/ContainerCards.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { fetchProductsFailure, fetchProductsRequest, fetchProductsSuccess } from '../redux/actions';
 
-const ContainerCards = () => {
+const ContainerCards = ({products,loading,error,fetchProducts}) => {
+  useEffect(() => {
+    fetchProducts();
+  },[fetchProducts])
+
+  if(loading) return <div>Cargando</div>
+  if(error) return <div>Error {error.message}</div>
+
   return (
     <section>
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
@@ -15,97 +25,51 @@ const ContainerCards = () => {
         </header>
 
         <ul className="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-          <li>
-            <Link to="/" className="group block overflow-hidden">
-              <img
-                src="https://via.placeholder.com/500"
-                alt=""
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-              />
+          {
+            products?.map(product => (
+              <li key={product.id}>
+                <Link to="/" className="group block overflow-hidden">
+                  <img
+                    src={product.image}
+                    alt=""
+                    className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  />
 
-              <div className="relative bg-white pt-3">
-                <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  camisa
-                </h3>
+                  <div className="relative bg-white pt-3">
+                    <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                      {product.title}
+                    </h3>
 
-                <p className="mt-2">
-                  <span className="sr-only"> Regular Price </span>
+                    <p className="mt-2">
+                      <span className="sr-only"> Regular Price </span>
 
-                  <span className="tracking-wider text-gray-900"> 200.000 </span>
-                </p>
-              </div>
-            </Link>
-          </li>
+                      <span className="tracking-wider text-gray-900"> {product.price} </span>
+                    </p>
+                  </div>
+                </Link>
+              </li>
 
-          <li>
-            <Link to="/" className="group block overflow-hidden">
-              <img
-                src="https://via.placeholder.com/500"
-                alt=""
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-              />
-
-              <div className="relative bg-white pt-3">
-                <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  camisa
-                </h3>
-
-                <p className="mt-2">
-                  <span className="sr-only"> Regular Price </span>
-
-                  <span className="tracking-wider text-gray-900"> 200.000 </span>
-                </p>
-              </div>
-            </Link>
-          </li>
-
-          <li>
-            <Link to="/" className="group block overflow-hidden">
-              <img
-                src="https://via.placeholder.com/500"
-                alt=""
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-              />
-
-              <div className="relative bg-white pt-3">
-                <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  camisa
-                </h3>
-
-                <p className="mt-2">
-                  <span className="sr-only"> Regular Price </span>
-
-                  <span className="tracking-wider text-gray-900"> 200.000 </span>
-                </p>
-              </div>
-            </Link>
-          </li>
-
-          <li>
-            <Link to="/" className="group block overflow-hidden">
-              <img
-                src="https://via.placeholder.com/500"
-                alt=""
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-              />
-
-              <div className="relative bg-white pt-3">
-                <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  camisa
-                </h3>
-
-                <p className="mt-2">
-                  <span className="sr-only"> Regular Price </span>
-
-                  <span className="tracking-wider text-gray-900"> 200.000 </span>
-                </p>
-              </div>
-            </Link>
-          </li>
+            ))
+          }
         </ul>
       </div>
     </section>
   )
 }
 
-export default ContainerCards
+const mapStateToProps = state => ({
+  products: state.products,
+  loading: state.loading,
+  error: state.error
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchProducts: () => {
+    dispatch(fetchProductsRequest());
+    axios.get("https://fakestoreapi.com/products")
+    .then(response => dispatch(fetchProductsSuccess(response.data)))
+    .catch(error => fetchProductsFailure(error))
+  }
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(ContainerCards)
