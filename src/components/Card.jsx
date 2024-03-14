42b953b0cf243e74f4cbbcda4fd4441b9926d60d
jhoancanchila <canchila.jhoan@gmail.com>
2024-03-14 12:05:35 -0500

    Cart(component-CartDetail):add new component CartDetail

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,13 +12,20 @@ const Card = ({image,title,price}) => {
         />
 
         <div className="relative bg-white pt-3">
-          <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+          <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4 lg:truncate">
             {title}
           </h3>
+          <div className="flex items-center justify-between">
 
-          <p className="mt-2">
-            <span className="tracking-wider text-gray-900"> {`USD ${price}`} </span>
-          </p>
+            <p className="mt-2">
+              <span className="tracking-wider text-gray-900"> {`USD ${price}`} </span>
+            </p>
+            <button
+                class="mt-1.5 inline-block bg-indigo-600 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+              >
+                Agregar
+            </button>
+          </div>
         </div>
       </Link>
     </li>
