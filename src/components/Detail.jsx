6f20fdd3eb875b0551bd3f11b0ac451216185210
jhoancanchila <canchila.jhoan@gmail.com>
2024-03-15 08:43:09 -0500

    structure(Detail):handle data product detail

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,29 +1,49 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useFetchProducts } from "../hooks";
+import { useEffect } from "react";
+import { handleAddTocart } from "../functions";
+import { useDispatch } from "react-redux";
 
 const Detail = () => {
-  const params = useParams();
+  const dispatch = useDispatch();
+  const { fetchProductById, loading, error, productAdded } = useFetchProducts("all");
+  const { id } = useParams();
+  
+  useEffect(()=>{
+    if(id){
+      fetchProductById(Number(id));
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+  
+  if(loading) return <div>Cargando...</div>
+  if(error) return <div>Error {error.message}</div>
+
   return (
     <section>
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
           <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
             <img
-              alt=""
-              src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="imagen-producto"
+              src={productAdded?.image}
               className="absolute inset-0 h-full w-full object-cover"
             />
           </div>
 
           <div className="lg:py-24">
-            <h2 className="text-3xl font-bold sm:text-4xl">product name</h2>
+            <h2 className="text-3xl font-bold sm:text-4xl">{productAdded?.title}</h2>
 
             <p className="mt-4 text-gray-600">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
-              eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
-              quidem quam repellat.
+              {
+                productAdded?.description
+              }
+            </p>
+            <p className="mt-2">
+              <span className="tracking-wider text-black-900"> {`USD ${productAdded?.price}`} </span>
             </p>
             <button
-              href="#"
+              onClick={() => handleAddTocart(dispatch,productAdded)}
               className="mt-8 inline-block bg-indigo-600 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white hover:bg-indigo-700"
             >
               Agregar
