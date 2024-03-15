f3156c728aec92850a4a8ca0159b60f0e6e64dce
jhoancanchila <canchila.jhoan@gmail.com>
2024-03-15 09:26:47 -0500

    fix(structure-Card):fix button add to car in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -25,22 +25,22 @@ const Card = ({ product }) => {
           <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4 lg:truncate">
             {title}
           </h3>
-          <div className="flex items-center justify-between">
-
-            <p className="mt-2">
-              <span className="tracking-wider text-gray-900"> {`USD ${price}`} </span>
-            </p>
-            <button
-              onClick={() => !productExist ? handleAddTocart(dispatch,product) : null}
-              className="mt-1.5 inline-block bg-indigo-600 hover:bg-indigo-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
-            >
-              {
-                productExist ? "Sacar del carrito" : "Agregar al carrito"
-              }
-            </button>
-          </div>
         </div>
       </Link>
+      <div className="flex items-center justify-between">
+
+        <p className="mt-2">
+          <span className="tracking-wider text-gray-900"> {`USD ${price}`} </span>
+        </p>
+        <button
+          onClick={() => !productExist ? handleAddTocart(dispatch,product) : null}
+          className="mt-1.5 inline-block bg-indigo-600 hover:bg-indigo-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+        >
+          {
+            productExist ? "Sacar del carrito" : "Agregar al carrito"
+          }
+        </button>
+      </div>
     </li>
   )
 }
