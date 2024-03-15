846c6612d469f8878c8f70c014c80de692d89199
jhoancanchila <canchila.jhoan@gmail.com>
2024-03-14 20:26:26 -0500

    Redux(actions,reducers):add actions and reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,19 @@
-import { ADD_TO_CART, FETCH_PRODUCTS_FAILURE, FETCH_PRODUCTS_REQUEST, FETCH_PRODUCTS_SUCCESS } from "./actions";
+import { 
+  ADD_TO_CART, 
+  FETCH_PRODUCTS_FAILURE, 
+  FETCH_PRODUCTS_REQUEST, 
+  FETCH_PRODUCTS_SUCCESS,
+  FETCH_PRODUCT_BY_ID_FAILURE,
+  FETCH_PRODUCT_BY_ID_REQUEST,
+  FETCH_PRODUCT_BY_ID_SUCCESS
+  } from "./actions";
 
 const initialState = {
   products: [],
   loading: false,
   error: null,
-  productsCart: []
+  productsCart: [],
+  productAdded: null
 };
 
 
@@ -23,6 +32,24 @@ const productsReducers = (state = initialState, action) => {
         products:action.payload
       }
     case FETCH_PRODUCTS_FAILURE:
+      
+      return {...state,
+        loading:false,
+        error:action.payload
+      }
+    case FETCH_PRODUCT_BY_ID_REQUEST:
+      
+      return {...state,
+        loading:true,
+        error:null
+      }
+    case FETCH_PRODUCT_BY_ID_SUCCESS:
+      
+      return {...state,
+        loading:false,
+        productAdded: action.payload
+      }
+    case FETCH_PRODUCT_BY_ID_FAILURE:
       
       return {...state,
         loading:false,
