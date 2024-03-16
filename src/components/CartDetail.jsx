e529e99ecac4e4ae821a89c00aef10b5cbfa8a30
jhoancanchila <canchila.jhoan@gmail.com>
2024-03-16 11:35:28 -0500

    Modal(structure):add img in method pay

--- src/components/CartDetail.jsx
+++ src/components/CartDetail.jsx
@@ -4,6 +4,8 @@ import { useDispatch } from "react-redux";
 import { Fragment, useEffect, useState } from "react";
 import masterLogo  from "../assets/master.png";
 import visaLogo  from "../assets/visa.png";
+import billeteraLogo  from "../assets/billetera.png";
+import pagoLogo  from "../assets/pago.png";
 import Modal from "./Modal";
 
 const CartDetail = () => {
@@ -196,101 +198,48 @@ const CartDetail = () => {
                 <h2 className="text-3xl font-bold sm:text-4xl">Seleccione método de pago</h2>
               </div>
                 <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
-                  <div className="flex items-start gap-4" onClick={()=>{ setSteps(1);localStorage.setItem("steps",1) }}>
-                    <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-                      <svg
-                        className="h-5 w-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                        <path
-                          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                        ></path>
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                        ></path>
-                      </svg>
-                    </span>
+                  <div className="cursor-pointer flex h-24 items-center rounded-2xl  gap-4" onClick={()=>{ setSteps(1);localStorage.setItem("steps",1) }}>
+                    <div className="w-16 h-24">
+                      <img src={masterLogo} alt="master" />
+                      <img src={visaLogo} alt="visa" />
+                    </div>
 
                     <div>
                       <h2 className="text-lg font-bold">Tarjeta de crédito</h2>
 
                       <p className="mt-1 text-sm text-gray-300">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-                        possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       </p>
                     </div>
                   </div>
-
-                  <div className="flex items-start gap-4">
-                    <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-                      <svg
-                        className="h-5 w-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                        <path
-                          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                        ></path>
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                        ></path>
-                      </svg>
-                    </span>
+                  <div className="cursor-pointer flex h-24 items-center rounded-2xl  gap-4">
+                    <div className="w-16 h-24">
+                      <img src={billeteraLogo} alt="billetera" />
+                    </div>
 
                     <div>
-                      <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+                      <h2 className="text-lg font-bold">Billetera electrónica</h2>
 
                       <p className="mt-1 text-sm text-gray-300">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-                        possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       </p>
                     </div>
                   </div>
-
-                  <div className="flex items-start gap-4">
-                    <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-                      <svg
-                        className="h-5 w-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                        <path
-                          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                        ></path>
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                        ></path>
-                      </svg>
-                    </span>
+                  <div className="cursor-pointer flex h-24 items-center rounded-2xl border-black gap-4">
+                    <div className="w-16 h-24">
+                      <img src={pagoLogo} alt="efectivo" />
+                    </div>
 
                     <div>
-                      <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+                      <h2 className="text-lg font-bold">En efectivo</h2>
 
                       <p className="mt-1 text-sm text-gray-300">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-                        possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       </p>
                     </div>
                   </div>
+
+                  
                   <button
                     onClick={handleCancelTransaction}
                     className="mt-1.5 inline-block bg-indigo-600 hover:bg-indigo-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
